4e15bc1247725b58f7fa95095d7a786b1cea685d
k0ur0x <kourox@protonmail.com>
2021-12-08 07:52:26 +0330

    Day 7: part 2

--- day_07/src/lib.rs
+++ day_07/src/lib.rs
@@ -10,6 +10,28 @@ pub fn least_fuel_position(positions: &mut [i32]) -> i32 {
     fuel
 }
 
+fn fuel_cost(p1: i32, p2: i32) -> i32 {
+    let distance = (p1 - p2).abs();
+
+    distance * (distance + 1) / 2
+}
+
+pub fn least_fuel_inc_position(positions: &[i32]) -> i32 {
+    let mut least_fuel = i32::MAX;
+
+    let max_pos = positions.iter().copied().max().unwrap();
+    for align_position in 0..max_pos {
+        let mut cost = 0;
+        for &position in positions {
+            cost += fuel_cost(position, align_position);
+        }
+
+        least_fuel = least_fuel.min(cost);
+    }
+
+    least_fuel
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -18,5 +40,16 @@ mod tests {
     fn test_least_fuel_position() {
         let mut positions = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
         assert_eq!(least_fuel_position(&mut positions), 37);
+    }
+
+    #[test]
+    fn test_fuel_cost() {
+        assert_eq!(fuel_cost(16, 5), 66);
+    }
+
+    #[test]
+    fn test_least_fuel_inc_position() {
+        let positions = vec![16, 1, 2, 0, 4, 2, 7, 1, 2, 14];
+        assert_eq!(least_fuel_inc_position(&positions), 168);
     }
 }
